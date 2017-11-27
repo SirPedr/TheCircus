fe0d2fe1cb970b470bdecd84c1db5f3afca546bc
Pedro Paulo <ppagbpedro@gmail.com>
2017-11-27 15:29:29 -0200

    Otimization blaster power 9000

--- scripts/tudo.js
+++ scripts/tudo.js
@@ -1,32 +1,34 @@
 // dá play e pause na musica quando se aperta o botão se som
+musicaFundoEl = document.querySelector('#musica-fundo');
+if(musicaFundoEl !== null){
+  let somEl = document.querySelector('#som');
+  let i = 0;
 
-let musicaFundoEl = document.querySelector('#musica-fundo');
-let somEl = document.querySelector('#som');
-let i = 0;
-
-somEl.addEventListener('click',function(){
-  i++;
-  if (i%2 == 0 ) {
-    musicaFundoEl.play();
-    somEl.src = "imgs/icones/som.png"
-  }else {
-    musicaFundoEl.pause();
-    somEl.src = "imgs/icones/mudo.png";
-  }
-});
+  somEl.addEventListener('click',function(){
+    i++;
+    if (i%2 == 0 ) {
+      musicaFundoEl.play();
+      somEl.src = "imgs/icones/som.png"
+    }else {
+      musicaFundoEl.pause();
+      somEl.src = "imgs/icones/mudo.png";
+    }
+  });
+}
 
 /* Janela Modal */
-
-let divEl = document.querySelector('#div-modal');
 let botaoI = document.querySelector('#exclamacao');
-let mascaraEl = document.querySelector('#mascara');
+if(botaoI !== null){
+  let divEl = document.querySelector('#div-modal');
+  let mascaraEl = document.querySelector('#mascara');
 
-botaoI.addEventListener('click', function(){
-  divEl.classList.add('aparece');
-  mascaraEl.classList.add('clicavel');
-});
+  botaoI.addEventListener('click', function(){
+    divEl.classList.add('aparece');
+    mascaraEl.classList.add('clicavel');
+  });
 
-mascaraEl.addEventListener('click',function(){
-      divEl.classList.remove('aparece');
-      mascaraEl.classList.remove('clicavel');
-});
+  mascaraEl.addEventListener('click',function(){
+    divEl.classList.remove('aparece');
+    mascaraEl.classList.remove('clicavel');
+  });
+}
